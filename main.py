ab39f83e767de912f7211450a401a9e20168b055
Ingenuity.exe <isjaw2018@gmail.com>
2026-07-27 18:56:11 +0200

    fix: add s3 client

--- main.py
+++ main.py
@@ -11,6 +11,15 @@ import os
 import shutil
 import boto3
 
+# Configuration S3
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name=os.getenv("AWS_REGION")
+)
+S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
+
 class PokemonCreate(BaseModel):
     id: int
     name: str
